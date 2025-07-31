f566d137d049bb0101e5b12a5dc5561543bfa7d2
Anuja ChaudhARI <167920706+AnujaChaudhari17@users.noreply.github.com>
2025-07-31 15:56:25 +0530

    Update decode_upperimm_inst.sv

--- 3_riscv/ver/decode_upperimm_inst.sv
+++ 3_riscv/ver/decode_upperimm_inst.sv
@@ -12,10 +12,31 @@ module decode_upperimm_inst(
 
 // Edit the code here begin ---------------------------------------------------
 
-    assign rd = 'b0;
-    assign imm = 'b0;
-    assign alu_control = 'b0;
-    
+    // assign rd = 'b0;
+    // assign imm = 'b0;
+    // assign alu_control = 'b0;
+    logic [6:0] opcode;
+
+    always_comb begin
+        // Default assignments
+        rd = instruction_code[11:7];
+        imm = {instruction_code[31:12], 12'b0};
+
+        
+        alu_control = 5'b0;
+        opcode = instruction_code[6:0];
+
+        case(opcode)
+            7'b0110111: begin // LUI
+                alu_control = `LUI;
+                
+            end
+            7'b0010111: begin // AUIPC
+                alu_control = `AUIPC;
+                
+            end
+        endcase
+    end
 // Edit the code here end -----------------------------------------------------
 
 /*
