cb5e165b0a6ef46c1f42de2c59d68b8b887eec59
Anuja ChaudhARI <167920706+AnujaChaudhari17@users.noreply.github.com>
2025-07-31 16:08:15 +0530

    Update prog_add_sub.sv

--- 1_combinational/prog_add_sub/prog_add_sub.sv
+++ 1_combinational/prog_add_sub/prog_add_sub.sv
@@ -9,10 +9,43 @@ module prog_add_sub#(
 );
 
 // Edit the code here begin ---------------------------------------------------
+ logic [DATA_WD-1:0] b_mod;
+    logic  c_in;
+    always_comb begin
+        if (i_mode == 0) begin
+            b_mod = i_b;
+            c_in  = 1'b0;
+        end else begin
+            b_mod = ~i_b;  
+            c_in  = 1'b1;
+        end
+    end
+    logic [DATA_WD:0] temp;
+    ripple_carry_adder #(.DATA_WD(DATA_WD)) u_adder (
+        .i_a(i_a),
+        .i_b(b_mod),
+        .i_c(c_in),
+        .o_arith_out(temp)
+        
+    );
+    always_comb begin
+        if(i_mode==1)begin
+            o_arith_out={1'b0,temp[3:0]};
+        end
+        else
+        o_arith_out=temp;
+    end
 
-	assign o_arith_out = 'b0;
-	assign o_ovr = 1'b0;
+    // Overflow detection
+    always_comb begin
+        o_ovr = 1'b0;
+        if (i_mode == 1 && (i_a < i_b)) begin
+            o_ovr = 1'b1;
+        end
+        
+    end
 
+    
 // Edit the code here end -----------------------------------------------------
 
 /*
