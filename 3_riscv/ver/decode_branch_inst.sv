4553f4403bfdb2ca48816f79fdc07609afb95e71
Anuja ChaudhARI <167920706+AnujaChaudhari17@users.noreply.github.com>
2025-07-31 15:52:27 +0530

    Update decode_branch_inst.sv

--- 3_riscv/ver/decode_branch_inst.sv
+++ 3_riscv/ver/decode_branch_inst.sv
@@ -13,10 +13,28 @@ module decode_branch_inst(
 
 // Edit the code here begin ---------------------------------------------------
 
-    assign rs1 = 'b0;
-    assign rs2 = 'b0;
-    assign imm = 'b0;
-    assign branch_control = 'b0;
+    // assign rs1 = 'b0;
+    // assign rs2 = 'b0;
+    // assign imm = 'b0;
+    // assign branch_control = 'b0;
+    logic [2:0] func3;
+        assign rs1 = instruction_code[19:15];
+        assign  rs2 = instruction_code[24:20];
+        assign imm = {instruction_code[31:31],instruction_code[7:7],instruction_code[30:25],instruction_code[11:8],1'b0};
+    always_comb begin
+      
+        func3 = instruction_code[14:12];
+        case(func3)
+        3'b000: branch_control = `BEQ;
+        3'b001 : branch_control = `BNE;
+        3'b100 : branch_control = `BLT;
+        3'b101: branch_control = `BGE;
+        3'b110: branch_control = `BLTU;
+        3'b111 : branch_control = `BGEU;
+        default : branch_control = 3'b0;
+
+        endcase
+    end
     
 // Edit the code here end -----------------------------------------------------
 
