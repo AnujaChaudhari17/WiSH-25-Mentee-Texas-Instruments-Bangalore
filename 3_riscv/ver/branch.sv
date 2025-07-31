267bf133c5365ab070a3bdd2937459b392c1c0d1
Anuja ChaudhARI <167920706+AnujaChaudhari17@users.noreply.github.com>
2025-07-31 15:51:37 +0530

    Update branch.sv

--- 3_riscv/ver/branch.sv
+++ 3_riscv/ver/branch.sv
@@ -18,9 +18,54 @@ module branch(
 
 // Edit the code here begin ---------------------------------------------------
 
-    assign pc_update_control = 'b0;
-    assign pc_update_val = 'b0;
-    assign ignore_curr_inst = 'b0;
+    // assign pc_update_control = 'b0;
+    // assign pc_update_val = 'b0;
+    // assign ignore_curr_inst = 'b0;
+    always@(posedge i_clk or negedge i_rst) begin
+        if(~i_rst) begin
+            ignore_curr_inst <= 1'b0;
+        end
+        else 
+    ignore_curr_inst <= pc_update_control;
+    end
+
+    always_comb begin
+        if(branch_control == `BEQ && rs1_val == rs2_val) begin
+            pc_update_control = 1'b1;
+            pc_update_val = pc + imm;
+        end
+        
+        else if( branch_control == `BNE && rs1_val != rs2_val ) begin
+            pc_update_control = 1'b1;
+            pc_update_val = pc + imm;
+
+        end
+        else if(branch_control == `BLT && $signed(rs1_val) < $signed(rs2_val)) begin
+            pc_update_control = 1'b1;
+            pc_update_val = pc + imm;
+        end
+        else if(branch_control == `BGE && $signed(rs1_val) >= $signed(rs2_val)) begin
+            pc_update_control = 1'b1;
+            pc_update_val = pc + imm;
+        end
+        else if(branch_control == `BLTU && rs1_val < rs2_val) begin
+            pc_update_control = 1'b1;
+            pc_update_val = pc + imm;
+        end
+        else if(branch_control == `BGEU && rs1_val >= rs2_val) begin
+            pc_update_control = 1'b1;
+            pc_update_val = pc + imm;
+    end
+    else if(branch_control == `BR_NOP ) begin
+        pc_update_control = 1'b0;
+         pc_update_val = 32'b0;
+    end
+
+    else begin
+    pc_update_control = 1'b0;
+    pc_update_val = 32'b0;
+    end
+    end
     
 // Edit the code here end -----------------------------------------------------
 
