43cad1d00c75b31f116c7422bff66687846e0596
Anuja ChaudhARI <167920706+AnujaChaudhari17@users.noreply.github.com>
2025-07-31 16:01:50 +0530

    Update mem.sv

--- 3_riscv/ver/mem.sv
+++ 3_riscv/ver/mem.sv
@@ -3,7 +3,7 @@ module mem(
     input logic i_clk,
     input logic i_rst,
     input logic [9:0] in_mem_addr,
-    input logic in_mem_re_web,
+    input logic in_mem_re_web,//read=1,write=0
     input logic [31:0] in_mem_write_data,
     input logic [3:0] in_mem_byte_en,
     output logic [31:0] out_mem_data
@@ -14,7 +14,28 @@ logic [31:0] write_data_muxed;
 
 // Edit the code here begin ---------------------------------------------------
 
-    assign out_mem_data = 'b0;
+    //assign out_mem_data = 'b0;
+    integer i;
+    always @(posedge i_clk or negedge i_rst) begin
+        if(i_rst==0)begin
+            out_mem_data<=32'b0;
+    end  
+    else begin
+        if(in_mem_re_web==1)begin
+            out_mem_data<=memory_reg[in_mem_addr];
+        end
+        else begin
+            for(i=0;i<4;i=i+1)begin
+                if(in_mem_byte_en[i]==1)begin
+                 memory_reg[in_mem_addr][ 8*i+:8]<=in_mem_write_data[ 8*i+:8];
+
+                end
+            end
+        end
+
+    end
+    end
+    
     
 // Edit the code here end -----------------------------------------------------
 
