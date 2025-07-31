fb65f9786cc26c500052dc55cf6efbd20c522b15
Anuja ChaudhARI <167920706+AnujaChaudhari17@users.noreply.github.com>
2025-07-31 15:55:15 +0530

    Update decode_reg_inst.sv

--- 3_riscv/ver/decode_reg_inst.sv
+++ 3_riscv/ver/decode_reg_inst.sv
@@ -13,11 +13,33 @@ module decode_reg_inst(
 
 // Edit the code here begin ---------------------------------------------------
 
-    assign rs1 = 'b0;
-    assign rs2 = 'b0;
-    assign rd = 'b0;
-    assign alu_control = 'b0;
-    
+    // assign rs1 = 'b0;
+    // assign rs2 = 'b0;
+    // assign rd = 'b0;
+    // assign alu_control = 'b0;
+    logic [9:0] temp;
+    always_comb begin
+        temp[9:0] = {instruction_code[14:12],instruction_code[31:25]};
+
+        rs1 = instruction_code[19:15];
+        rs2 = instruction_code[24:20];
+        rd =  instruction_code[11:7];
+        case(temp)
+        10'b0: alu_control=  `ADD;
+        10'b0000100000: alu_control = 5'd2;
+        10'b1000000000: alu_control = 5'd3;
+        10'b1100000000: alu_control = 5'd4;
+        10'b1110000000: alu_control = 5'd5;
+        10'b0010000000: alu_control = 5'd6;
+        10'b1010000000: alu_control = 5'd7;
+        10'b1010100000: alu_control = 5'd8;
+        10'b0100000000: alu_control = 5'd9;
+        10'b0110000000: alu_control = 5'd10;
+        default: alu_control =5'b0;
+        endcase;
+
+        
+    end
 // Edit the code here end -----------------------------------------------------
 
 /*
