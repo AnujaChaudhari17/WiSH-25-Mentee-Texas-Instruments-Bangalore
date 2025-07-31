e6c88936f681bb2c272a9579241ba76ca1661924
Anuja ChaudhARI <167920706+AnujaChaudhari17@users.noreply.github.com>
2025-07-31 15:54:28 +0530

    Update decode_load_inst.sv

--- 3_riscv/ver/decode_load_inst.sv
+++ 3_riscv/ver/decode_load_inst.sv
@@ -13,10 +13,28 @@ module decode_load_inst(
 
 // Edit the code here begin ---------------------------------------------------
 
-    assign rs1 = 'b0;
-    assign rd = 'b0;
-    assign imm = 'b0;
-    assign load_control = 'b0;
+    // assign rs1 = 'b0;
+    // assign rd = 'b0;
+    // assign imm = 'b0;
+    // assign load_control = 'b0;
+    logic[2:0] func3;
+
+    
+    always_comb begin
+        rs1 = instruction_code[19:15];
+        rd   = instruction_code[11:7];
+        imm = instruction_code[31:20];
+        func3 = instruction_code[14:12];
+        case(func3)
+        3'b000: load_control = `LB;
+        3'b001: load_control = `LH;
+        3'b010: load_control = `LW;
+        3'b100: load_control = `LBU;
+        3'b101: load_control = `LHU;
+        default: load_control = 3'b0;
+        endcase
+        
+    end
     
 // Edit the code here end -----------------------------------------------------
 
@@ -32,3 +50,4 @@ module decode_load_inst(
 `endif
 
 endmodule
+
