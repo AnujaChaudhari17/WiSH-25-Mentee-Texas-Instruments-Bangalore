6d01c1eaf447d77421de808801736bb176f30674
Anuja ChaudhARI <167920706+AnujaChaudhari17@users.noreply.github.com>
2025-07-31 15:45:50 +0530

    Update seq_detect.sv

--- 2_sequential/seq_detect/seq_detect.sv
+++ 2_sequential/seq_detect/seq_detect.sv
@@ -3,13 +3,58 @@
 module seq_detect (
     input   logic           i_clk,
     input   logic           i_rstn,
-    input   logic           i_data,
-    output  logic           o_detect
+    input   logic           i_data,//i_data,
+    output  logic           o_detect//o_detect
 );
 
 // Edit the code here begin ---------------------------------------------------
 
-    assign o_detect = 'b0;
+
+   logic [2:0]y;
+   logic [2:0]Y;
+   localparam  A=3'b000;
+   localparam  B=3'b001;
+   localparam  C=3'b010;
+   localparam  D=3'b011;
+   localparam  E=3'b100;
+   always@(*)begin
+    case(y)
+    A: begin if(i_data==1'b0)
+        Y=A;
+        else
+        Y=B;
+    end
+    B: begin if(i_data==1'b0)
+        Y=A;
+        else
+        Y=C;
+    end
+    C: begin if(i_data==1'b0)
+        Y=A;
+        else
+        Y=D;
+    end
+    D:begin if(i_data==1'b0)
+        Y=A;
+        else
+        Y=E;
+    end
+    E: begin if(i_data==1'b0)
+        Y=A;
+        else
+        Y=E;
+    end
+    default:Y=3'b000;
+    endcase
+   end
+   always @(posedge i_clk or negedge i_rstn)begin
+    if(i_rstn==1'b0)begin
+        y<=A;
+    end
+    else
+    y<=Y;
+   end
+   assign o_detect=(y==E);
 
 // Edit the code here end -----------------------------------------------------
 
@@ -25,3 +70,5 @@ module seq_detect (
 `endif
 
 endmodule
+
+  
