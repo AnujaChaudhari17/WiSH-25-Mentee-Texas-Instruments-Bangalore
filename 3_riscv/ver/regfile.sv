255afbb93d0208fdc49a9fc5ca55a34a86dd9649
Anuja ChaudhARI <167920706+AnujaChaudhari17@users.noreply.github.com>
2025-07-31 16:03:16 +0530

    Update regfile.sv

--- 3_riscv/ver/regfile.sv
+++ 3_riscv/ver/regfile.sv
@@ -13,10 +13,32 @@ module regfile(
 
 logic [31:0] regs [0:31];
 
+
 // Edit the code here begin ---------------------------------------------------
 
-    assign rs1_val = 'b0;
-    assign rs2_val = 'b0;
+    //assign rs1_val = 'b0;
+    //assign rs2_val = 'b0;
+    always @ (posedge i_clk or negedge i_rst)begin
+        if(i_rst==0) begin
+           for(int i=0;i<32;i=i+1) begin
+                regs[i] <= 32'b0;
+            end
+        end
+        else if(rd_write_control && rd!=5'd0) begin
+           
+            regs[rd] <= rd_write_val;
+             $display("WRITE: rd = %0d, val = %0d, en = %b", rd, rd_write_val, rd_write_control);
+
+        end
+        end
+    always_comb begin
+    
+        rs1_val=regs[rs1];
+        rs2_val=regs[rs2];
+        end
+        
+    
+    
     
 // Edit the code here end -----------------------------------------------------
 
